0d83918f1676fc67b9c00402958c114f67feea0a
Sacheth <158269498+ksacheth@users.noreply.github.com>
2026-06-26 19:30:29 +0530

    fix: Adjust lodash import to directly target `lodash/capitalize` and add `@types/lodash` to devDependencies.

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 // This file imports `lodash`, but `lodash` is NOT listed in package.json.
 // On Vercel the build fails with: "Module not found: Can't resolve 'lodash'".
 // The correct fix is to add "lodash" to dependencies in package.json.
-import { capitalize } from "lodash";
+import capitalize from "lodash/capitalize";
 
 export default function Home() {
   const title = capitalize("hello from the deploymind test app");
